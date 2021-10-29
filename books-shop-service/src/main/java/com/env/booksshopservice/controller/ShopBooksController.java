be42102c0581a07d11c741ff6544d5f8ace1efbe
Nicoleta Corocea <nicoleta_corocea@yahoo.com>
2021-10-29 15:12:18 +0300

    add logging to everything

--- books-shop-service/src/main/java/com/env/booksshopservice/controller/ShopBooksController.java
+++ books-shop-service/src/main/java/com/env/booksshopservice/controller/ShopBooksController.java
@@ -1,13 +1,20 @@
 package com.env.booksshopservice.controller;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShopBooksController {
 
+    private static final Logger log = LogManager.getLogger(ShopBooksController.class);
+
     @GetMapping("/shop")
     public String shop() {
+        log.log(Level.INFO, "shop was called");
+
         return "Les Mis";
     }
 }
