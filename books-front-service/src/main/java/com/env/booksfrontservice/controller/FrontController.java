7a7377ae37db87583e0026a4b9038f4ac6e60065
Nicoleta Corocea <nicoleta_corocea@yahoo.com>
2021-11-04 13:16:55 +0200

    front controller improvement

--- books-front-service/src/main/java/com/env/booksfrontservice/controller/FrontController.java
+++ books-front-service/src/main/java/com/env/booksfrontservice/controller/FrontController.java
@@ -2,6 +2,8 @@ package com.env.booksfrontservice.controller;
 
 import com.env.booksfrontservice.client.BookStoreClient;
 import com.env.booksfrontservice.client.ReviewServiceClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FrontController {
 
+    private static final Logger log = LogManager.getLogger(FrontController.class);
 
     @Autowired
     private BookStoreClient bookStoreClient;
@@ -18,6 +21,12 @@ public class FrontController {
 
     @GetMapping("/books")
     public String getAllBooksWithReviews() {
-        return bookStoreClient.shop() + " - " + reviewServiceClient.getReview("les-mis");
+        String result = "bla bla";
+        try {
+            result = bookStoreClient.shop() + " - " + reviewServiceClient.getReview("les-mis");
+        } catch (Exception ex) {
+           log.error(ex);
+        }
+        return result;
     }
 }
